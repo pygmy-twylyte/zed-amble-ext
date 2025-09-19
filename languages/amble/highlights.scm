d226b5d8f258ea30ef19e1387641b1938c119d65
David Brown <davidjvbrown.md@gmail.com>
2025-09-18 19:53:26 -0500

    Highlighting completed for trigger "when" conditions.

--- languages/amble/highlights.scm
+++ languages/amble/highlights.scm
@@ -1,17 +1,22 @@
+; Highlights file for Amble DSL
 (comment) @comment
 
+; ROOMS Highlights
+; Basic / Required Elements
 (room_def "room" @keyword)
 (room_def id: (room_id) @label)
 (room_name "name" @keyword name: (room_name) @string)
 (room_desc ["desc" "description"] @keyword desc: (room_desc) @string)
 (room_visited "visited" @keyword visited: (room_visited) @boolean)
 
+; Exits +/= conditional blocks
 (room_exit "exit" @keyword dir: (exit_dir) @variant "->" @punctuation.special dest: (exit_dest) @label)
 (exit_stmt ["hidden" "locked"] @keyword)
 (required_items_stmt "required_items" @function item_id: (identifier) @label)
 (required_flags_stmt "required_flags" @function flag_id: (identifier) @label)
 (barred_stmt "barred" @keyword msg: (barred_msg) @string)
 
+; Overlays -- singular and set builders
 (overlay_stmt "overlay" @keyword "if" @keyword)
 (ovl_text_stmt "text" @keyword text: (ovl_text) @string)
 (ovl_flag_status ["flag" "set" "unset" "complete"] @function flag_name: (identifier) @label)
@@ -30,3 +35,46 @@
 (ovl_npc_state_set ["overlay" "if" "npc"] @function npc_id: (identifier) @label "here" @function)
 (npc_state_set_line text: (string) @string)
 (npc_state_set_custom "custom" @constant state: (identifier) @string.special)
+
+
+; TRIGGERS Highlights
+(trigger_def ["trigger" "when"] @keyword )
+(trigger_def name: (string) @string)
+(trigger_def once: (only_once_kw) @property)
+
+; when conditions (events)
+(when_cond) @keyword
+(always_event) @type
+(enter_room ["enter" "room"] @type room_id: (_) @label)
+(take_item ["take" "item"] @type item_id: (_) @label)
+(drop_item ["drop" "item"] @type item_id: (_) @label)
+(unlock_item ["unlock" "item"] @type item_id: (_) @label)
+(talk_to_npc ["talk" "to" "npc"] @type npc_id: (_) @label)
+(open_item ["open" "item"] @type item_id: (_) @label)
+(leave_room ["leave" "room"] @type room_id: (_) @label)
+(look_at_item ["look" "at" "item"] @type item_id: (_) @label)
+(use_item ["use" "item" "ability"] @type
+    item_id: (_) @label
+    ability: (_) @variable.special
+)
+(give_to_npc ["give" "item" "to" "npc"] @type
+    item_id: (_) @label
+    npc_id: (_) @label
+)
+(use_item_on_item
+    ["use" "item" "on" "item" "interaction"] @type
+    tool_id: (_) @label
+    target_id: (_) @label
+    interaction: (_) @variable.special
+)
+(act_on_item ["act" "on" "item"] @type
+    action: (_) @variable.special
+    item_id: (_) @label
+)
+(take_from_npc ["take" "item" "from" "npc"] @type
+    item_id: (_) @label
+    npc_id: (_) @label
+)
+(insert_item_into ["insert" "item" "into"] @type
+    item_id: (_) @label
+)
